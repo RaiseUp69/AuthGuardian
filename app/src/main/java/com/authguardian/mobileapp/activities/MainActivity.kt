b729c7a8a34a4c882af34e4307ed567ed581156c
Bohdan Oliinyk <bogdasin321@gmail.com>
2023-04-19 16:20:01 +0300

    Added nav ui navigation between fragments

--- app/src/main/java/com/authguardian/mobileapp/activities/MainActivity.kt
+++ app/src/main/java/com/authguardian/mobileapp/activities/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.authguardian.mobileapp.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.authguardian.mobileapp.R
 
 class MainActivity : AppCompatActivity() {
+
+    // region Data
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    // endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
         setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) = item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    override fun onSupportNavigateUp() = navController.navigateUp(appBarConfiguration)
 }
