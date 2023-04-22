9b415dc6ef030865bfc2028a797ad262c81f4ee6
Bohdan <bohdan.oliinyk@ekreative.com>
2023-04-22 18:48:46 +0300

    Test commit from work notebook

--- app/src/main/java/com/authguardian/mobileapp/utils/NsdServiceUtils.kt
+++ app/src/main/java/com/authguardian/mobileapp/utils/NsdServiceUtils.kt
@@ -5,7 +5,7 @@ import android.content.Context
 class NsdServiceUtils(private val context: Context) {
 
     companion object {
-
+        // From work notebook
         val TAG: String = NsdServiceUtils::class.java.simpleName
         const val NSD_AUTH = "NSD_AUTH"
         const val NSD_SERVICE_TYPE = "_nsdchat._tcp"
