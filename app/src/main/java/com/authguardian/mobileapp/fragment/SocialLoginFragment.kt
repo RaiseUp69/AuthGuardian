ca8c40a563f9a025e768d1eff9ce7512ddd12f92
Bohdan <bohdan.oliinyk@ekreative.com>
2023-05-23 00:17:59 +0300

    Added a google sign in files and dependencies

---
+++ app/src/main/java/com/authguardian/mobileapp/fragment/SocialLoginFragment.kt
@@ -0,0 +1,31 @@
+package com.authguardian.mobileapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.authguardian.mobileapp.databinding.FragmentSocialLoginBinding
+import com.authguardian.mobileapp.viewmodel.SocialLoginViewModel
+
+class SocialLoginFragment : Fragment() {
+
+    private val viewModel: SocialLoginViewModel by viewModels()
+
+    private var _binding: FragmentSocialLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSocialLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
