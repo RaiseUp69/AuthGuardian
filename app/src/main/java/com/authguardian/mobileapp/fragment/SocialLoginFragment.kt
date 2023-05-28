4bacb657198edb7659b9fac099fee6e7f30aa13a
Bohdan <bohdan.oliinyk@ekreative.com>
2023-05-28 20:06:04 +0300

    Added a google sign in button, refactor

--- app/src/main/java/com/authguardian/mobileapp/fragment/SocialLoginFragment.kt
+++ app/src/main/java/com/authguardian/mobileapp/fragment/SocialLoginFragment.kt
@@ -6,15 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.authguardian.mobileapp.R
 import com.authguardian.mobileapp.databinding.FragmentSocialLoginBinding
 import com.authguardian.mobileapp.viewmodel.SocialLoginViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
-class SocialLoginFragment : Fragment() {
+
+class SocialLoginFragment : Fragment(), View.OnClickListener {
 
     private val viewModel: SocialLoginViewModel by viewModels()
 
     private var _binding: FragmentSocialLoginBinding? = null
     private val binding get() = _binding!!
+
+    private var googleSignInClient: GoogleSignInClient? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +32,33 @@ class SocialLoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val googleSignIn = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignIn)
+
+        with(binding) {
+            with(googleSignInButton) {
+                setSize(SignInButton.SIZE_ICON_ONLY)
+                setOnClickListener(this@SocialLoginFragment)
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.googleSignInButton -> {
+                viewModel.onGoogleSignInClicked()
+            }
+        }
     }
 }
